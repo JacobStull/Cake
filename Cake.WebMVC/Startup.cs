39ea36b42741b22e4f304bffbfb6216912ecd06f
JacobStull <jrstull00@gmail.com>
2021-05-23 06:07:27 -0400

    Add project files.

---
+++ Cake.WebMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Cake.WebMVC.Startup))]
+namespace Cake.WebMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
